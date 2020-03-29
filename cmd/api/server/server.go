0db4156a47700297d31bc240ed5ed08d98701cc1
daniel <aciddaniel@gmail.com>
2020-03-29 22:01:19 +0300

    making server part of pkg, configurable and reusable

--- cmd/api/server/server.go
+++
@@ -1,7 +0,0 @@
-package server
-
-import "log"
-
-func Start() {
-	log.Println("Starting server")
-}
---
+++ pkg/server/server.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type Server struct {
+	srv *http.Server
+}
+
+func New() *Server {
+	return &Server{
+		srv: &http.Server{},
+	}
+}
+
+func (s *Server) WithAddr(addr string) *Server {
+	s.srv.Addr = addr
+	return s
+}
+
+func (s *Server) WithErrLogger(l *log.Logger) *Server {
+	s.srv.ErrorLog = l
+	return s
+}
+
+func (s *Server) WithRouter(router *httprouter.Router) *Server {
+	s.srv.Handler = router
+	return s
+}
+
+func (s *Server) Start() error {
+	if len(s.srv.Addr) == 0 {
+		return errors.New("Server missing address")
+	}
+
+	if s.srv.Handler == nil {
+		return errors.New("Server missing handler")
+	}
+
+	return s.srv.ListenAndServe()
+}
+
+func (s *Server) Close() error {
+	return s.srv.Close()
+}
