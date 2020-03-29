7698613d637d3e8ce6adf342c2767f2ed5eb67e5
daniel <aciddaniel@gmail.com>
2020-03-29 22:28:22 +0300

    assembling all new bits together

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"log"
 
+	"github.com/boilerplate/cmd/api/router"
 	"github.com/boilerplate/pkg/application"
 	"github.com/boilerplate/pkg/exithandler"
+	"github.com/boilerplate/pkg/logger"
+	"github.com/boilerplate/pkg/server"
 	"github.com/joho/godotenv"
 )
 
@@ -18,7 +21,24 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
+	srv := server.
+		New().
+		WithAddr(app.Cfg.GetAPIPort()).
+		WithRouter(router.Get(app)).
+		WithErrLogger(logger.Error)
+
+	go func() {
+		logger.Info.Printf("starting server at %s", app.Cfg.GetAPIPort())
+		if err := srv.Start(); err != nil {
+			log.Fatal(err.Error())
+		}
+	}()
+
 	exithandler.Init(func() {
+		if err := srv.Close(); err != nil {
+			log.Println(err.Error())
+		}
+
 		if err := app.DB.Close(); err != nil {
 			log.Println(err.Error())
 		}
