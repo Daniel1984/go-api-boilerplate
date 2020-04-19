b48ea1ec56e1b6f6dedee1a1fe267a3394f88b7a
daniel <aciddaniel@gmail.com>
2020-04-19 23:08:22 +0300

    create user handler

---
+++ cmd/api/handlers/createuser/createuser.go
@@ -0,0 +1,36 @@
+package createuser
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/boilerplate/cmd/api/models"
+	"github.com/boilerplate/pkg/application"
+	"github.com/boilerplate/pkg/middleware"
+	"github.com/julienschmidt/httprouter"
+)
+
+func createUser(app *application.Application) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		defer r.Body.Close()
+
+		user := &models.User{}
+		json.NewDecoder(r.Body).Decode(user)
+
+		if err := user.Create(r.Context(), app); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Oops")
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		response, _ := json.Marshal(user)
+		w.Write(response)
+	}
+}
+
+func Do(app *application.Application) httprouter.Handle {
+	mware := []middleware.Middleware{middleware.LogRequest(app)}
+	return middleware.Chain(createUser(app), mware...)
+}
