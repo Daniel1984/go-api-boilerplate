eae28d8790682874797ca062a6d7e67301d5148d
daniel <aciddaniel@gmail.com>
2020-04-19 23:32:11 +0300

    removed redundant code from logging mdw

--- pkg/middleware/logging.go
+++ pkg/middleware/logging.go
@@ -3,16 +3,13 @@ package middleware
 import (
 	"net/http"
 
-	"github.com/boilerplate/pkg/application"
 	"github.com/boilerplate/pkg/logger"
 	"github.com/julienschmidt/httprouter"
 )
 
-func LogRequest(app *application.Application) Middleware {
-	return func(next httprouter.Handle) httprouter.Handle {
-		return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-			logger.Info.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
-			next(w, r, p)
-		}
+func LogRequest(next httprouter.Handle) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		logger.Info.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+		next(w, r, p)
 	}
 }
