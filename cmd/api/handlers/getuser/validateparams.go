03b5e684727b40edbf8e818b32131321e6a150fc
daniel <aciddaniel@gmail.com>
2020-04-19 22:47:07 +0300

    get user validate params local middleware

---
+++ cmd/api/handlers/getuser/validateparams.go
@@ -0,0 +1,28 @@
+package getuser
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/boilerplate/cmd/api/models"
+	"github.com/julienschmidt/httprouter"
+)
+
+func validateParams(next httprouter.Handle) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		uid := p.ByName("id")
+
+		id, err := strconv.Atoi(uid)
+		if err != nil {
+			w.WriteHeader(http.StatusPreconditionFailed)
+			fmt.Fprintf(w, "malformed id")
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), models.CtxKey("userid"), id)
+		r = r.WithContext(ctx)
+		next(w, r, p)
+	}
+}
