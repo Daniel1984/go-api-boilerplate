6c0f77c8d90dd32ae7c20b40da6812d5e0cb1baa
Toul <touldeguiacranmer@gmail.com>
2021-01-31 13:23:08 -0600

    Update entrypoint.dev.sh

--- scripts/entrypoint.dev.sh
+++ scripts/entrypoint.dev.sh
@@ -7,6 +7,10 @@ go run cmd/dbmigrate/main.go
 echo "#################### migrating test db"
 go run cmd/dbmigrate/main.go -dbname=boilerplatetest
 
+echo "################### removing the .git dir"
+
+rm -rf .git
+
 echo "#################### downloading CompileDaemon"
 # disable go modules to avoid this package from getting into go.mod
 # as we only need it locally to watch and rebuild server on change
